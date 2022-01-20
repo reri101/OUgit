fcc698c2e309e2c4fa29323d82e2d5a38ed774dc
240847@edu.p.lodz.pl <2404847@edu.p.lodz.pl>
2022-01-20 15:51:25 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ougit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ougit.dir/main.cpp.obj"
+  "libougit.dll.a"
+  "ougit.exe"
+  "ougit.exe.manifest"
+  "ougit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ougit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
